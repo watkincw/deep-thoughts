06fb16cc4bb093d3c45b07e6efd0accd3a6ae53d
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-10 18:55:36 -0500

    created add user and login mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
@@ -24,6 +25,28 @@ const resolvers = {
         // get a single thought by (thought)_id
         thought: async (parent, { _id }) => {
             return Thought.findOne({ _id });
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            
+            return user;
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            return user;
         }
     }
 };
