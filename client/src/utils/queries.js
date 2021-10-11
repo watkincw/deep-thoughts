fed30caa4d82510d4dcbeede9e830a4abc019610
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-11 18:35:35 -0500

    added FriendList to profile page

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,26 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_USER = gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            friendCount
+            friends {
+                _id
+                username
+            }
+            thoughts {
+                _id
+                thoughtText
+                createdAt
+                reactionCount
+            }
+        }
+    }
+`;
+
 export const QUERY_THOUGHTS = gql`
     query thoughts($username: String) {
         thoughts(username: $username) {
