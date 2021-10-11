80816932da777e1259625401fe1a9de1cda1ae76
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-11 16:43:19 -0500

    21.3.6 - Set up full-stack structure with client-side React App

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Home from './pages/Home';
 
 
 const httpLink = createHttpLink({
-	uri: 'http://localhost:3001/graphql',
+	uri: '/graphql',
 });
 
 const client = new ApolloClient({
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -33,6 +35,17 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
 	app.listen(PORT, () => {
