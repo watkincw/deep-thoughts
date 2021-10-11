e33906d528623b9f69f3d99a65c25daf511776bd
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-10 19:15:54 -0500

    added authorization to logins

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -28,11 +29,14 @@ const resolvers = {
         }
     },
     Mutation: {
+        // create a new user
         addUser: async (parent, args) => {
             const user = await User.create(args);
+            const token = signToken(user);
             
-            return user;
+            return { token, user };
         },
+        // log an existing user in, allowing them to "edit themselves" (update an existing user)
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
@@ -46,7 +50,8 @@ const resolvers = {
                 throw new AuthenticationError('Incorrect credentials');
             }
 
-            return user;
+            const token = signToken(user);
+            return { token, user };
         }
     }
 };
