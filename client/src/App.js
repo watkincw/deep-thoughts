cb4654f230b0ec2ca29f2bea03c247f44f18fc69
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-11 18:14:00 -0500

    added navigation and SingleThought pathing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
+// pages
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import NoMatch from './pages/NoMatch';
+import Profile from './pages/Profile';
+import SingleThought from './pages/SingleThought';
+
+// components
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -19,13 +28,40 @@ const client = new ApolloClient({
 function App() {
 	return (
 		<ApolloProvider client={ client }>
-			<div className="flex-column justify-flex-start min-100-vh">
-				<Header />
-				<div className="container">
-					<Home />
+			<Router>
+				<div className="flex-column justify-flex-start min-100-vh">
+					<Header />
+					<div className="container">
+						<Switch>
+							<Route 
+								exact path="/" 
+								component={ Home } 
+							/>
+							<Route 
+								exact path="/login" 
+								component={ Login } 
+							/>
+							<Route 
+								exact path="/signup" 
+								component={ Signup } 
+							/>
+							<Route 
+								exact path="/profile/:username?" 
+								component={ Profile } 
+							/>
+							<Route 
+								exact path="/thought/:id" 
+								component={ SingleThought } 
+							/>
+
+							<Route
+								component={ NoMatch }
+							/>
+						</Switch>
+					</div>
+					<Footer />
 				</div>
-				<Footer />
-			</div>
+			</Router>
 		</ApolloProvider>
 	);
 }
