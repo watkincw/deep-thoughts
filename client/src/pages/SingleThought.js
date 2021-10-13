4276be616dafb9369844a6f64cda9f86a05d5b4c
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-13 17:54:14 -0500

    created ADD_REACTION functionality

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
+import Auth from '../utils/auth';
 
 // components
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
 	const { id: thoughtId } = useParams();
@@ -34,6 +36,7 @@ const SingleThought = props => {
 			</div>
 
 			{ thought.reactionCount > 0 && <ReactionList reactions={ thought.reactions } />}
+			{ Auth.loggedIn() && <ReactionForm thoughtId={ thought._id } /> }
 		</div>
 	);
 };
