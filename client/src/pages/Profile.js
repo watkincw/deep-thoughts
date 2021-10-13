0c6fcce23cf2412a0286ab6d8614c0b483487034
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-13 16:19:19 -0500

    created addFriend Button and function

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,13 +5,18 @@ import Auth from '../utils/auth';
 // components
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
+
+import { useQuery, useMutation } from '@apollo/client';
 
 // queries
-import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+// mutations
+import { ADD_FRIEND } from '../utils/mutations';
 
 const Profile = () => {
 	const { username: userParam } = useParams();
+	const [addFriend] = useMutation(ADD_FRIEND);
 
 	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
 		variables: { username: userParam }
@@ -23,12 +28,12 @@ const Profile = () => {
 	if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
 		return <Redirect to="/profile" />;
 	}
-	console.log(data);
 	console.log(Auth);
-	console.log(Auth.getProfile());
-	console.log(Auth.getProfile().data);
-	console.log(Auth.getProfile().data.username);
-	console.log(Auth.loggedIn());
+	// console.log(Auth.getProfile());
+	// console.log(Auth.getProfile().data.username); // why is '.data' 'unknown'
+	// console.log(Auth.loggedIn());
+	console.log(data);
+	console.log(Auth.getProfile().data); // why is '.data' 'unknown'
 	console.log(userParam);
 	
 	if (loading) {
@@ -43,12 +48,28 @@ const Profile = () => {
 		);
 	}
 
+	// on 'Add Friend' button click...
+	const handleClick = async () => {
+		try {
+		  	await addFriend({
+				variables: { id: user._id }
+		  	});
+		} catch (e) {
+		  	console.error(e);
+		}
+	};
+
 	return (
 		<div>
 			<div className="flex-row mb-3">
 				<h2 className="bg-dark text-secondary p-3 display-inline-block">
 					Viewing { userParam ? `${user.username}'s` : 'your' } profile.
 				</h2>
+				{ userParam && (
+					<button className="btn ml-auto" onClick={ handleClick }>
+						Add Friend
+					</button>
+				) }
 			</div>
 
 			<div className="flex-row justify-space-between mb-3">
@@ -64,6 +85,8 @@ const Profile = () => {
 					/>
 				</div>
 			</div>
+
+			<div className="mb-3">{ !userParam && <ThoughtForm /> }</div>
 		</div>
 	);
 };
