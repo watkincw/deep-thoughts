768e4183d69cc90b7b406e99a7463aa97b94b525
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-13 16:22:10 -0500

    display ThoughtForm on home page if logged in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
@@ -11,17 +12,27 @@ const Home = () => {
 	const { loading, data } = useQuery(QUERY_THOUGHTS);
 	// use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
 	const { data: userData } = useQuery(QUERY_ME_BASIC);
-
+	// console.log(userData);
+	// console.log(loading);
+	// console.log(data);
+	
 	const thoughts = data?.thoughts || [];
-	console.log(thoughts);
-	console.log(data); // remove - here for troubleshooting
-	console.log(userData); // remove - here for troubleshooting
-
+	// console.log(thoughts);
+	// console.log(data); // remove - here for troubleshooting
+	// console.log(userData); // remove - here for troubleshooting
+	
 	const loggedIn = Auth.loggedIn();
+	// console.log(loggedIn);
+	// console.log(Auth);
 
 	return (
 		<main>
 			<div className='flex-row justify-space-between'>
+				{ loggedIn && (
+					<div className="col-12 mb-3">
+						<ThoughtForm />
+					</div>
+				) }
 				<div className={ `col-12 mb-3 ${ loggedIn && 'col-lg-8' }` }>
 					{ loading ? (
 						<div>Loading...</div>
